ae96062b6c0c7b5699cb6f23fff63bec7a6ab917
zhanggd16816 <zhanggd16816@hundsun.com>
2016-11-22 00:18:46 +0800

    配置文件修改

---
+++ graduation/src/com/graduation/web/db/dao/DaoTest.java
@@ -0,0 +1,19 @@
+/**
+ * @Project: graduation
+ * @Title: DaoTest.java
+ * @Package com.graduation.web.db.dao
+ * @Description: TODO(用一句话描述)
+ * @author zhanggd16816 zhanggd16816@hundsun.com
+ * @date 2016年11月22日 上午12:17:54
+ * @Copyright: 2016 www.hundsun.com All Rights Reserved.
+ * @version V1.0.0.0 
+ */
+package com.graduation.web.db.dao;
+
+ /**
+ * @Description: TODO(用一句话描述)
+ * @date 2016年11月22日 上午12:17:54
+ */
+public class DaoTest {
+
+}
---
+++ graduation/src/com/graduation/web/db/model/ModelTest.java
@@ -0,0 +1,19 @@
+/**
+ * @Project: graduation
+ * @Title: ModelTest.java
+ * @Package com.graduation.web.db.model
+ * @Description: TODO(用一句话描述)
+ * @author zhanggd16816 zhanggd16816@hundsun.com
+ * @date 2016年11月22日 上午12:18:19
+ * @Copyright: 2016 www.hundsun.com All Rights Reserved.
+ * @version V1.0.0.0 
+ */
+package com.graduation.web.db.model;
+
+ /**
+ * @Description: TODO(用一句话描述)
+ * @date 2016年11月22日 上午12:18:19
+ */
+public class ModelTest {
+
+}
